c39e03098392d0a0eee659421b5a63df3d6e6c0a
Carlos Eduardo Lourenço De Souza <97536308+carloseduardolsouza@users.noreply.github.com>
2023-12-06 13:49:50 -0300

    pause no projeto
    criado box de ofertas

--- src/screens/homePrincipal/homePrincipal.jsx
+++ src/screens/homePrincipal/homePrincipal.jsx
@@ -1,11 +1,23 @@
 import HeaderPrincipal from "../../components/headerPrincipal/headerPrincipal";
 import MenuHorizontal from "../../components/menuHorizontal/menuHorizontal";
+import ItensOferta from "../../components/ItensOfertas/itensOfertas";
+
+import './homePrincipalCss.css'
 
 function homePrincipal() {
     return ( 
         <div>
             <HeaderPrincipal/>
             <MenuHorizontal/>
+            <div className="main">
+                <ItensOferta/>
+                <ItensOferta/>
+                <ItensOferta/>
+                <ItensOferta/>
+                <ItensOferta/>
+                <ItensOferta/>
+                <ItensOferta/>
+            </div>
         </div>
      );
 }
